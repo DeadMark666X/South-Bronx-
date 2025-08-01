8da7263749fa16ee3845971a42d29d78e6f3fc8a
Markx Hub <adityamahardikaputra4@gmail.com>
2025-08-01 14:02:02 +0900

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -1,4 +1,4 @@
-local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
+local OrionLib =loadstring(game:HttpGet("https://raw.githubusercontent.com/1nig1htmare1234/SCRIPTS/main/Orion.lua"))()
 local Window = OrionLib:MakeWindow({
     Name = "YoxanHub | South Bronx",
     HidePremium = false,
